3616759bf7c74dcb8d554e666e404415cfb32d02
Luthfiyanto <luthfiyantooo@students.unnes.ac.id>
2024-01-18 22:38:55 +0700

    initial project

---
+++ src/App.jsx
@@ -0,0 +1,27 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <header>
+        <nav>
+          <li>
+            <a href=""></a>
+          </li>
+          <li>
+            <a href=""></a>
+          </li>
+          <li>
+            <a href=""></a>
+          </li>
+          <li>
+            <a href=""></a>
+          </li>
+        </nav>
+      </header>
+      <body></body>
+    </>
+  );
+}
+
+export default App;
